821b922d719474b8f3eaf8fd48012c834a7326d3
PK <pradeepdhital124@gmail.com>
2022-12-30 00:27:57 +1100

    last commit

--- src/components/TaskLine.js
+++ src/components/TaskLine.js
@@ -9,21 +9,28 @@ export const TaskLine = () => {
         <thead>
           <tr>
             <th>
-                <Form.Check aria-label="Checkbox for following text input" />
+              <Form.Check aria-label="Checkbox for following text input" />
             </th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Username</th>
+            <th>Task</th>
+            <th>Hrs</th>
+            <th>Action</th>
           </tr>
         </thead>
         <tbody>
           <tr>
             <td>
-                <Form.Check aria-label="Checkbox for following text input" />
+              <Form.Check aria-label="Checkbox for following text input" />
             </td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
+            <td>Netflix</td>
+            <td>6</td>
+            <td>
+              <button>
+                <i className="fa-solid fa-arrow-right"></i>
+              </button>
+              <button>
+                <i className="fa-solid fa-arrow-left"></i>
+              </button>
+            </td>
           </tr>
         </tbody>
       </Table>
